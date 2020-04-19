a83fc9ddd7f96dffb078eaa187dcdf4574d17f4f
LesleySong <vibiasong@163.com>
2020-04-19 14:28:26 +0800

    0419-1

---
+++ js/blog.js
@@ -0,0 +1,22 @@
+var blogItem = new Vue({
+	el : "#blogItem",
+	data:{
+		blogTitle = [
+			{'Title':'A Title For Test A'},
+			{'Title':'A Title For Test B'},
+			{'Title':'A Title For Test C'},
+			{'Title':'A Title For Test D'},
+			{'Title':'A Title For Test E'}
+		]
+	}
+}) 
+
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      {message: 'foo' },
+      {message: 'Bar' }
+    ]
+  }
+})
